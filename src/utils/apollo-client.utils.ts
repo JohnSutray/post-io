9b520ab0dbdbb7f96c4d6af04a4241e05a62321f
John Sutray <JohnSutray@gmail.com>
2019-10-16 23:02:11 +0300

    4

--- src/utils/apollo-client.utils.ts
+++ src/utils/apollo-client.utils.ts
@@ -1,29 +1,12 @@
-import { createHttpLink } from 'apollo-link-http';
+import { HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { environment } from '../environments/environment';
-import { ApolloLink, from } from 'apollo-link';
 
 export class ApolloClientUtils {
-  static apolloOptionsFactory() {
+  static apolloOptionsFactory(httpLink: HttpLink) {
     return {
       cache: new InMemoryCache(),
-      link: from([
-        new ApolloLink((operation, forward) => {
-          return forward(operation).map(response => {
-            const context = operation.getContext();
-            const headers = context.response.headers;
-
-            console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaa');
-            console.log(context.response);
-            console.log(Array.from(headers.keys()));
-
-            return response;
-          });
-        }),
-        createHttpLink({
-          uri: environment.graphQLApiEndPoint,
-        }),
-      ]),
+      link: httpLink.create({uri: environment.graphQLApiEndPoint}),
     };
   }
 }
